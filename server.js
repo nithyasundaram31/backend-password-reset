600be4a03be0db9ac6ea6783c80e4ab0ec4dcfd3
nithya <nithukitty112@gmail.com>
2025-05-15 22:12:40 +0530

    changs

--- server.js
+++ server.js
@@ -10,43 +10,35 @@ dotenv.config();
 const app = express();
 
 
-/*app.use(cors({
-origin: "https://6819a21cba11b05832a97256--soft-sunshine-020931.netlify.app",
-credentials: true, 
-}));*/
-
-/*const allowedOrigins = [
-  'http://localhost:5173',
-  //'https://6819a21cba11b05832a97256--soft-sunshine-020931.netlify.app'
-  'https://681b62b63ee14a0008e5a887--soft-sunshine-020931.netlify.app'
-];*/
-
 app.use(cors({
-  origin: 'https://curious-puffpuff-49dc22.netlify.app',
+  origin: 'https://splendorous-gecko-55b2d1.netlify.app', //  frontend domain
   credentials: true,
 }));
 
 
+app.options('*', cors());
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
 app.get('/', (req, res) => {
-res.send('Password reset API is live');
+  res.send('Password reset API is live');
 });
 
-//Middleware to log requests
+// Middleware to log requests
 app.use(logger);
 
+// Routes
 app.use('/api/auth', authRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-console.log('✅ Connected to MongoDB');
-app.listen(process.env.PORT || 4000, '0.0.0.0', () =>
-console.log(`🚀 Server running on port ${process.env.PORT || 4000}`)
-);
 
-})
-.catch(err => console.error('❌ MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('✅ Connected to MongoDB');
+    app.listen(process.env.PORT || 4000, '0.0.0.0', () =>
+      console.log(`🚀 Server running on port ${process.env.PORT || 4000}`)
+    );
+  })
+  .catch(err => console.error('❌ MongoDB connection error:', err));
